810e2b2ad6ddb13aea241745fa91c1ae478905f6
Ian Springer <ian.springer@bvaccel.com>
2019-05-01 17:16:21 -0700

    Updates

---
+++ precache-manifest.1be0ff62da39904338634fac17df6651.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/IanSpringer.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "18e9fc2964097d3b1b9f8a285dfb5a92",
+    "url": "/IanSpringer.github.io/static/media/Qardoos.18e9fc29.otf"
+  },
+  {
+    "revision": "e9416be8de33d8bbb643",
+    "url": "/IanSpringer.github.io/static/js/runtime~main.efa3da8a.js"
+  },
+  {
+    "revision": "e666c9db3a2e20e53796",
+    "url": "/IanSpringer.github.io/static/js/main.f6f3723a.chunk.js"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/IanSpringer.github.io/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "e666c9db3a2e20e53796",
+    "url": "/IanSpringer.github.io/static/css/main.9bfefb34.chunk.css"
+  },
+  {
+    "revision": "61fc90700d9a5869758d92c56eaf51eb",
+    "url": "/IanSpringer.github.io/index.html"
+  }
+];
